2ab090b77aa77ec771dabb9c22c372fd7e5d2ca9
cosmin-marginean <cos@hey.com>
2022-12-28 09:06:20 +0000

    Update to snapshot, cleanup test

--- src/test/kotlin/com/test/BugTest.kt
+++ src/test/kotlin/com/test/BugTest.kt
@@ -2,8 +2,8 @@ package com.test
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
 
 @MicronautTest
 class BugTest {
@@ -13,6 +13,8 @@ class BugTest {
 
     @Test
     fun test() {
-        documentRepository.findByTags("tag")
+        documentRepository.save(Document("john", listOf("a", "b")))
+        documentRepository.save(Document("anne", listOf("a", "c")))
+        assertEquals(2, documentRepository.findByTagsCollectionContains("a").size)
     }
 }
